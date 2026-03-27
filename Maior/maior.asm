d6c35c19caba0c5e9589b3b2e9bcb5d002bc3eb5
Prof. Me. Ricardo de Andrade Kratz <rakratz@gmail.com>
2026-03-27 09:29:54 -0300

    Add files via upload

---
+++ Maior/maior.asm
@@ -0,0 +1,21 @@
+section .data
+    msg_maior db "O primeiro numero e maior", 10, 0
+    tam_maior equ $ - msg_maior
+    msg_menor db "O primeiro numero e menor", 10, 0
+    tam_menor equ $ - msg_menor
+    msg_igual db "Os numeros sao iguais", 10, 0
+    tam_igual equ $ - msg_igual 
+
+    msg1 db "Digite o primeiro numero: ", 0
+    tam1 equ $ - msg1
+    msg2 db "Digite o segundo numero: ", 0
+    tam2 equ $ - msg2
+
+section .bss
+    num1 resb 10
+    num2 resb 10
+
+section .text
+    global _start
+
+_start:
