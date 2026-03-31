29b73e9610b92d57ea3ebb19dd1688cae266b490
Prof. Me. Ricardo de Andrade Kratz <rakratz@gmail.com>
2026-03-31 10:14:26 -0300

    Add files via upload

---
+++ Contador/contador.asm
@@ -0,0 +1,49 @@
+section .data
+    msg_fim db "Fim da contagem!", 10
+    tam_fim equ $ - msg_fim
+
+section .bss
+    num resb 2   ; número + '\n'
+
+section .text
+    global _start
+
+_start:
+    mov r8, 10          ; começa em 9 (ASCII simples)
+
+loop_contador:
+
+    ; converte número para ASCII
+    mov rax, r8
+    add rax, '0'
+    mov [num], al
+
+    ; quebra de linha
+    mov byte [num+1], 10
+
+    ; imprime número
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, num
+    mov rdx, 2
+    syscall
+
+    ; decrementa
+    dec r8
+
+    ; continua enquanto r8 > 0
+    cmp r8, 0
+    jg loop_contador
+
+; mensagem final
+fim:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg_fim
+    mov rdx, tam_fim
+    syscall
+
+; encerra programa
+    mov rax, 60
+    xor rdi, rdi
+    syscall
